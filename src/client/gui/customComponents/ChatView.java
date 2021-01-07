d83b722b60885ec082d44614f6dc5f800f338ff7
Tobaeas10 <tobias.weiss16@gmail.com>
2021-01-07 22:16:14 +0100

    - Chatting basics are done. Next up: receive and ACTUALLY send messages... Bennet

--- src/client/gui/customComponents/ChatView.java
+++ src/client/gui/customComponents/ChatView.java
@@ -6,11 +6,16 @@ import client.data.Client;
 import client.data.Message;
 import client.gui.AppView;
 import client.gui.customComponents.borderless.BorderlessScene;
+import javafx.collections.ListChangeListener;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+
+import java.time.LocalDateTime;
 
 /*
 Used to represent minimally a chat graphically by using information from private Chat chat
@@ -22,16 +27,21 @@ public class ChatView extends VBox {
     private Client client;
     private AppView appView;
 
-
-    //ChatNavigationList.focusedBackground
+    private static final CornerRadii DEFAULT_CORNER_RADII = new CornerRadii(6);
     private static final Background DEFAULT = new Background(new BackgroundFill(AppView.SLIGHT_HIGHLIGHT_COLOR.desaturate().desaturate().brighter(), CornerRadii.EMPTY, Insets.EMPTY));
+    private static final Background DEFAULT_ROUNDED = new Background(new BackgroundFill(AppView.SLIGHT_HIGHLIGHT_COLOR, DEFAULT_CORNER_RADII, Insets.EMPTY));
+    private static final Background TEXT_FROM_CLIENT = new Background(new BackgroundFill(AppView.SLIGHT_HIGHLIGHT_COLOR.saturate().darker(), DEFAULT_CORNER_RADII, Insets.EMPTY));
+
     public final ChatMessagesView chatMessagesView;
+    private final ColorPicker userColorPicker;
 
     public ChatView(Chat chat, Client client, AppView appView, BorderlessScene scene) {
 
         setChat(chat);
         setClient(client);
         setAppView(appView);
+
+        getChat().setChatView(this);
 
         setStyle("-fx-border-style: solid inside;" +
                  "-fx-border-color: lightgrey;");
@@ -41,15 +51,13 @@ public class ChatView extends VBox {
         scene.setMoveControl(chatToolBar);
         getChildren().add(chatToolBar);
 
-        getChildren().add(AppView.defaultSeparator());
-
         Rectangle rectangle = new Rectangle(60d / 2d, 60d / 1.3d);
 
         StackPane stack = new StackPane();
         stack.setMaxSize(rectangle.getWidth(), rectangle.getHeight());
         chatToolBar.getItems().add(stack);
 
-        ColorPicker userColorPicker = new ColorPicker();
+        userColorPicker = new ColorPicker();
         userColorPicker.setFocusTraversable(false);
         userColorPicker.setMinHeight(rectangle.getHeight());
         userColorPicker.valueProperty().bindBidirectional(chat.getColorProperty());
@@ -66,10 +74,35 @@ public class ChatView extends VBox {
 
         chatMessagesView = new ChatMessagesView();
         getChildren().add(chatMessagesView);
+
+        HBox writeMessageRoot = new HBox();
+        writeMessageRoot.setAlignment(Pos.CENTER_LEFT);
+        writeMessageRoot.setFillHeight(true);
+        getChildren().add(writeMessageRoot);
+
+        WriteMessageTextArea writeMessageTextArea = new WriteMessageTextArea() {
+            @Override
+            public void onEnter() {
+                appView.getController().sendMessage(this, chat.getUserName());
+            }
+        };
+
+        writeMessageTextArea.setPrefWidth(42069);
+        writeMessageRoot.getChildren().add(writeMessageTextArea);
+        writeMessageRoot.minHeightProperty().bind(writeMessageTextArea.minHeightProperty());
+
+        Button sendMessageButton = new Button("Send");
+        sendMessageButton.setMinWidth(70);
+        sendMessageButton.setOnAction(e -> appView.getController().sendMessage(writeMessageTextArea, chat.getUserName()));
+        writeMessageRoot.getChildren().add(sendMessageButton);
+
+        getChildren().add(AppView.slimSeparator());
     }
 
 
     private class ChatMessagesView extends ScrollPane {
+
+        private Message lastBuiltMessage; //not last sent message
 
         public ChatMessagesView() {
             super();
@@ -78,25 +111,97 @@ public class ChatView extends VBox {
             setFitToWidth(true);
 
             VBox root = new VBox();
-
+            root.setAlignment(Pos.TOP_CENTER);
+            root.setSpacing(2);
+            root.setPadding(new Insets(2, 0, 10, 0));
             setContent(root);
 
-            for (Message message : getChat().getMessages()) {
-                HBox cell = new HBox(getChat());
-//            cell.hoverProperty().addListener(e -> {
-//                cell.setBackground(focusedBackground);
-//            });
+            chat.getMessages().addListener((ListChangeListener<Message>) c -> {
+                for(Message message : c.getAddedSubList()) {
+                    System.out.println(message);
+                }
+            });
 
-                cell.setBackground(DEFAULT);
-                cell.setPadding(new Insets(3, 3, 3, 3));
-                cell.setSpacing(8);
+            for (Message message : getChat().getMessages()) {
+                LocalDateTime t2 = message.getTimeSend();
+                boolean dateStampAdded = false;
+                if(lastBuiltMessage != null) {
+                    LocalDateTime t1 = lastBuiltMessage.getTimeSend();
+                    if(t1.getDayOfYear() != t2.getDayOfYear() && t1.getYear() == t2.getYear()) {
+                        Label label = new Label(t2.format(AppView.DAY_MONTH_YEAR));
+                        label.setBackground(DEFAULT_ROUNDED);
+                        root.getChildren().add(label);
+                        dateStampAdded = true;
+                    }
+                } else {
+                    Label label = new Label(t2.format(AppView.DAY_MONTH_YEAR));
+                    label.setBackground(DEFAULT_ROUNDED);
+                    root.getChildren().add(label);
+                }
+
+                if(!dateStampAdded && lastBuiltMessage != null) {
+                    if(!lastBuiltMessage.getFrom().equals(message.getFrom())
+                                || lastBuiltMessage.getTimeSend().isBefore(message.getTimeSend().minusHours(2))) {
+                        root.getChildren().add(AppView.defaultSeparator());
+                    }
+                }
+
+                HBox cell = new HBox();
+                cell.setSpacing(2);
                 root.getChildren().add(cell);
+
+                Label time = new Label(message.getTimeSend().format(AppView.HOUR_MINUTE));
+                int fontSize = 12;
+                time.setStyle("-fx-font-size: " + fontSize);
+                time.setMinWidth(fontSize * 2.5);
 
                 GrowingTextArea textArea = new GrowingTextArea(message.getText());
                 textArea.maxWidthProperty().bind(cell.widthProperty());
-                cell.getChildren().add(textArea);
+                textArea.hoverProperty().addListener(e -> {
+                    if(textArea.isHover()) cell.setBackground(DEFAULT);
+                    else cell.setBackground(null);
+                });
+
+
+                if(message.getFrom().equals(client.getName())) {
+                    //if sender == this user: right
+                    textArea.setPadding(new Insets(0, 5, 0, 2));
+                    textArea.setBackground(TEXT_FROM_CLIENT);
+
+                    cell.setAlignment(Pos.CENTER_RIGHT);
+                    cell.widthProperty().addListener(e -> {
+                        cell.setPadding(new Insets(0, 3, 0, getWidth() * 0.2));
+                    });
+                    cell.getChildren().addAll(textArea, time); //left
+                } else {
+                    //left
+                    textArea.setPadding(new Insets(0, 2, 0, 5));
+                    userColorPicker.valueProperty().addListener(e -> {
+                        textArea.setBackground(getHSApprBackground(userColorPicker.getValue()));
+                    });
+                    textArea.setBackground(getHSApprBackground(getChat().getColor()));
+
+                    cell.setAlignment(Pos.CENTER_LEFT);
+                    cell.widthProperty().addListener(e -> {
+                        cell.setPadding(new Insets(0, getWidth() * 0.2, 0, 3));
+                    });
+                    cell.getChildren().addAll(time, textArea); //right
+                }
+
+                lastBuiltMessage = message;
+            }
+        }
+
+        private Background getHSApprBackground(Color color) {
+            Color approximationColor = AppView.SLIGHT_HIGHLIGHT_COLOR.saturate().darker();
+            while(color.getBrightness() < approximationColor.getBrightness() - 0.2) color = color.brighter();
+            while(color.getBrightness() > approximationColor.getBrightness() + 0.2) color = color.darker();
+            if(color.getSaturation() > 0) {
+                while(color.getSaturation() < approximationColor.getSaturation() - 0.1) color = color.saturate();
+                while(color.getSaturation() > approximationColor.getSaturation() + 0.1) color = color.desaturate();
             }
 
+            return new Background(new BackgroundFill(color, DEFAULT_CORNER_RADII, Insets.EMPTY));
         }
     }
 
@@ -111,12 +216,9 @@ public class ChatView extends VBox {
             setWrapText(true);
             setEditable(false);
             setFocusTraversable(false);
-            focusedProperty().addListener(e -> {
-                if(!isFocused()) deselect();
-            });
             setPromptText("[Empty Message]");
-            setPadding(new Insets(0, 2, 0, 5));
             setPrefWidth(42069);
+            focusedProperty().addListener(e -> setFocused(false));
         }
 
         @Override
@@ -128,21 +230,18 @@ public class ChatView extends VBox {
         }
 
         private void callWithLayout() {
-            layoutDone = true;
+            //Getting text instance
             ScrollPane scrollPane = (ScrollPane) lookup(".scroll-pane");
             scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
             scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-
             StackPane viewport = (StackPane) scrollPane.lookup(".viewport");
-
             Region content = (Region) viewport.lookup(".content");
             content.setPadding(new Insets(0));
-
             text = (Text) content.lookup(".text");
 
             //Width
             setWrapText(false);
-            double textWidth = text.getBoundsInLocal().getWidth() + 9;
+            double textWidth = text.getBoundsInLocal().getWidth() + 10;
 
             if (textWidth < DEFAULT_WIDTH) {
                 textWidth = DEFAULT_WIDTH;
@@ -167,6 +266,8 @@ public class ChatView extends VBox {
                 setPrefHeight(textHeightl);
                 setMaxHeight(textHeightl);
             });
+
+            layoutDone = true;
         }
     }
 
